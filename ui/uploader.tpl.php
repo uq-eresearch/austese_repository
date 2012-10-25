118294b7376da1f1480e4c4c27d081804c2db96b
Anna Gerber <anna.m.gerber@gmail.com>
2012-10-25 16:21:06 +1000

    web pages for uploader UI

---
+++ ui/uploader.tpl.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="/ext-4.1.1a/resources/css/ext-all.css">
+<link rel="stylesheet" href="/ext-4.1.1a/resources/css/ext-all-gray.css">
+<div id="uploaderui"></div>
+<div id="images"></div>
+
+
