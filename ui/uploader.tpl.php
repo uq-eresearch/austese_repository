0101b48c6e8cbe9f6c54edfc3887e36380a1f220
Anna Gerber <anna.m.gerber@gmail.com>
2012-11-19 15:13:55 +1000

    switch to extjs in /sites/all/libraries path

--- ui/uploader.tpl.php
+++ ui/uploader.tpl.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="/ext-4.1.1a/resources/css/ext-all.css">
-<link rel="stylesheet" href="/ext-4.1.1a/resources/css/ext-all-gray.css">
+<link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all.css">
+<link rel="stylesheet" href="/sites/all/libraries/ext-4.1.1a/resources/css/ext-all-gray.css">
 <div id="uploaderui"></div>
 <div id="images"></div>
 
