a37fbfb14487e2ebf0768f16d947a46b9d2e4061
Anna Gerber <anna.m.gerber@gmail.com>
2013-07-30 14:35:20 +1000

    Updated to codemirror 3.15 Enabled addons for tag matching, autoclosing of tags, tag folding, autocompletion (with some basic TEI tags configured)

--- ui/js/resourceedit.js
+++ ui/js/resourceedit.js
@@ -3,12 +3,111 @@ jQuery(function(){
      editor.loadResource(jQuery('#savebtn').data("resource"));
      jQuery('#savebtn').click(editor.newResourceVersion);
  });
+var generalElements = ["div","p","lb","l","pb","hi","list","note"];
+var generalAttrs = {"xml:id":null,"n":null,"xml:lang":null,"rend":null};
+var divAttrs = generalAttrs;
+divAttrs.type=null;
+var tags = {
+   "!top": ["TEI", "div"],
+   TEI: {
+       children:["teiHeader","text"]
+   },
+   teiHeader: {
+       children:["fileDesc"]
+   },
+   text: {
+       children:["body", "front","back"]
+   },
+   fileDesc:{
+       children:["titleStmt","publicationStmt","sourceDesc"]
+   },
+   titleStmt: {
+       children:["title","respStmt"]
+   },
+   respStmt: {
+       children:["resp","name"]
+   },
+   div: {
+       children: generalElements,
+       attrs: divAttrs
+   },
+   front: {children:generalElements},
+   back: {children:generalElements},
+   body: {children:generalElements},
+   list: {
+       children: ["item"],
+       attrs: generalAttrs
+   },
+   p: {
+       attrs: generalAttrs
+   },
+   l: {
+       attrs: generalAttrs
+   },
+   lb: {
+       attrs: generalAttrs
+   },
+   pb: {
+       attrs: generalAttrs
+   },
+   hi: {
+       attrs: generalAttrs
+   },
+   note: {
+       attrs: generalAttrs
+   },
+   item: {
+       attrs: generalAttrs
+   }
+};
 var editor = {
+ foldXML: function(cm, where) { 
+            cm.foldCode(where, CodeMirror.tagRangeFinder); 
+ },
+ completeAfter: function(cm, pred) {
+     var cur = cm.getCursor();
+     if (!pred || pred()) setTimeout(function() {
+       if (!cm.state.completionActive)
+         CodeMirror.showHint(cm, CodeMirror.hint.xml, {schemaInfo: tags, completeSingle: false});
+     }, 100);
+     return CodeMirror.Pass;
+ },
+
+ completeIfAfterLt: function(cm) {
+     return completeAfter(cm, function() {
+       var cur = cm.getCursor();
+       return cm.getRange(CodeMirror.Pos(cur.line, cur.ch - 1), cur) == "<";
+     });
+ },
+ completeIfInTag: function(cm) {
+     return completeAfter(cm, function() {
+       var tok = cm.getTokenAt(cm.getCursor());
+       if (tok.type == "string" && (!/['"]/.test(tok.string.charAt(tok.string.length - 1)) || tok.string.length == 1)) return false;
+       var inner = CodeMirror.innerMode(cm.getMode(), tok.state).state;
+       return inner.tagName;
+     });
+ },
  init : function(){
      editor.cm = CodeMirror.fromTextArea(document.getElementById("editor"), {
-         mode: {name: "xml", alignCDATA: true},         
-         lineNumbers: true
+         mode: {
+             name: "xml", 
+             alignCDATA: true
+         },   
+         autoCloseTags: true,
+         lineWrapping: true,
+         lineNumbers: true,
+         matchTags: true,
+         extraKeys: {
+             "'<'": editor.completeAfter,
+             "'/'": editor.completeIfAfterLt,
+             "' '": editor.completeIfInTag,
+             "'='": editor.completeIfInTag,
+             "Ctrl-Space": function(cm) {
+               CodeMirror.showHint(cm, CodeMirror.hint.xml, {schemaInfo: tags});
+             }
+         }
      }); 
+     editor.cm.on("gutterClick", editor.foldXML);
  },
  loadResource : function(uri) {
      if (uri){
@@ -36,6 +135,7 @@ var editor = {
  },
  newResourceVersion : function(){
      var uri = jQuery('#savebtn').data("resource");
+     jQuery('.alert').hide();
      if (uri){
          // create a new resource version
          jQuery.ajax({
