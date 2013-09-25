4aae51000dc94c38f83484da90141bad921dd99c
Anna Gerber <anna.m.gerber@gmail.com>
2013-09-25 10:12:43 +1000

    add floating save button for transcription editor resolves uq-eresearch/AustESE#188

--- ui/resources_edit.tpl.php
+++ ui/resources_edit.tpl.php
@@ -40,6 +40,15 @@ if (isset($project)) {
   <span id="failMessage"></span>
 </div>
 <div id="editInfo"></div>
+<div class="form-horizontal">
+<div class="sticky-bottom well">
+    <div class="pull-right">
+       <button data-resource="/<?php print drupal_get_path('module', 'repository');?>/api/resources/<?php print arg(3); ?>" class="btn btn-primary savebtn" title="Save as new version">Save</button>
+       <a href="/<?php print $modulePrefix; ?>/resources/<?php if ($existingId): print $existingId; endif; ?><?php if ($project): print "?project=".$project; endif; ?>">
+       <input type="button" class="btn" value="Cancel"></a>
+     </div>
+  </div>
+  </div>
 <!--  ui for multi/merge editor -->
 <div id="multi-editor-ui">
   <div class="row-fluid">
