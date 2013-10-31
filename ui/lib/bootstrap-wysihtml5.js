c28d86706ec2310f2f2f65ded1a8508c20648219
Anna Gerber <anna.m.gerber@gmail.com>
2013-10-31 15:09:14 +1000

    Used advanced markup parser rules in HTML editor

--- ui/lib/bootstrap-wysihtml5.js
+++ ui/lib/bootstrap-wysihtml5.js
@@ -393,64 +393,8 @@
         "link": true,
         "image": true,
         events: {},
-        parserRules: {
-            classes: {
-                // (path_to_project/lib/css/wysiwyg-color.css)
-                "wysiwyg-color-silver" : 1,
-                "wysiwyg-color-gray" : 1,
-                "wysiwyg-color-white" : 1,
-                "wysiwyg-color-maroon" : 1,
-                "wysiwyg-color-red" : 1,
-                "wysiwyg-color-purple" : 1,
-                "wysiwyg-color-fuchsia" : 1,
-                "wysiwyg-color-green" : 1,
-                "wysiwyg-color-lime" : 1,
-                "wysiwyg-color-olive" : 1,
-                "wysiwyg-color-yellow" : 1,
-                "wysiwyg-color-navy" : 1,
-                "wysiwyg-color-blue" : 1,
-                "wysiwyg-color-teal" : 1,
-                "wysiwyg-color-aqua" : 1,
-                "wysiwyg-color-orange" : 1
-            },
-            tags: {
-                "b":  {},
-                "i":  {},
-                "br": {},
-                "ol": {},
-                "ul": {},
-                "li": {},
-                "h1": {},
-                "h2": {},
-                "h3": {},
-                "h4": {},
-                "h5": {},
-                "h6": {},
-                "blockquote": {},
-                "u": 1,
-                "img": {
-                    "check_attributes": {
-                        "width": "numbers",
-                        "alt": "alt",
-                        "src": "url",
-                        "height": "numbers"
-                    }
-                },
-                "a":  {
-                    check_attributes: {
-                        'href': "url", // important to avoid XSS
-                        'target': 'alt',
-                        'rel': 'alt'
-                    }
-                },
-                "span": 1,
-                "div": 1,
-                // to allow save and edit files with code tag hacks
-                "code": 1,
-                "pre": 1
-            }
-        },
-        stylesheets: ["./lib/css/wysiwyg-color.css"], // (path_to_project/lib/css/wysiwyg-color.css)
+        parserRules: wysihtml5ParserRules,
+        stylesheets: [], 
         locale: "en"
     };
 
