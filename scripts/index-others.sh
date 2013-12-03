216eeba50b59a05416735077e72b0f11178efd90
Damien Ayers <damien@omad.net>
2013-12-03 16:18:19 +1000

    Update indexing scripts

--- scripts/index-others.sh
+++ scripts/index-others.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # A simple script that sends a bunch of POST requests to a locally running
 # ElasticSearch server with included mongodb-river, telling it about
@@ -6,11 +6,28 @@
 #
 # WARNING: Starts by deleting your existing ElasticSearch Austese Index
 
-curl -X DELETE localhost:9200/austese
+read -p "Do you wish to delete your ElasticSearch 'austese' index? " yn
+case $yn in
+    [Yy]* ) curl -X DELETE localhost:9200/austese;;
+    [Nn]* ) ;;
+    * ) ;;
+esac
+
+
 
 for collection in artefacts agents versions events works
 do
-read -d '' json_doc <<EOF
+  echo Deleting $collection river: 
+  curl -X DELETE localhost:9200/_river/$collection; echo
+
+done
+
+echo "Pausing 10 seconds while ElasticSearch catches up"
+sleep 10
+
+for collection in artefacts agents versions events works
+do
+  read -d '' json_doc <<EOF
 {
   "type": "mongodb",
   "mongodb": {
@@ -19,7 +36,7 @@ read -d '' json_doc <<EOF
       { "host": "127.0.0.1", "port": "27017" }
     ],
     "options": {
-      "include_fields": ["metadata"]
+      "include_fields": ["metadata", "_deleted"]
     },
     "db": "test",
     "collection": "$collection",
@@ -28,16 +45,13 @@ read -d '' json_doc <<EOF
   },
   "index": {
     "name": "austese",
-    "type": "$collection"
+    "type": "$collection",
+    "bulk": { "concurrent_requests": 1 }
   }
 }
 EOF
 
-echo Deleting $collection river: 
-curl -X DELETE localhost:9200/_river/$collection
-echo Deleting $collection index type
-curl -X DELETE localhost:9200/austese/$collection
-echo Creating $collection River
-curl -XPUT "localhost:9200/_river/$collection/_meta" -d"$json_doc"
-echo
+  echo Creating $collection River
+  curl -XPUT "localhost:9200/_river/$collection/_meta" -d"$json_doc"
+  echo
 done
